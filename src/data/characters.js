a9567ba27b10926f81489fec4e3f6a28586c11c8
Joao Vitor Freitas <jvfreitas02@gmail.com>
2022-07-27 03:49:07 -0300

    replace src data file

--- src/data/characters.js
+++ src/data/characters.js
@@ -11,7 +11,7 @@ const characters = [
     ],
     bio: 'Omen, uma lembrança fantasmagórica, caça nas sombras. Ele cega os inimigos, teleporta-se pelo campo e deixa a paranoia assumir o controle enquanto o adversário tenta descobrir de onde virá seu próximo ataque.',
     avatar:
-      'https://raw.githubusercontent.com/JV-Freitas/assets/main/valorant-player-valorant-omen-card.png',
+      'https://raw.githubusercontent.com/JV-Freitas/valorant-agents/main/public/assets/valorant-player-valorant-omen-card.png',
     isRadiant: true,
   },
   {
@@ -26,7 +26,7 @@ const characters = [
     ],
     bio: 'Chegando com tudo diretamente do Reino Unido, o poder estelar de Phoenix reluz em seu estilo de luta, incendiando o campo de batalha com luz e estilo. Tendo ajuda ou não, ele entra na luta como e quando achar que deve.',
     avatar:
-      'https://raw.githubusercontent.com/JV-Freitas/assets/main/valorant-player-valorant-phoenix-card.png',
+      'https://raw.githubusercontent.com/JV-Freitas/valorant-agents/main/public/assets/valorant-player-valorant-phoenix-card.png',
     isRadiant: true,
   },
   {
@@ -41,7 +41,7 @@ const characters = [
     ],
     bio: 'Como uma verdadeira fortaleza chinesa, Sage traz segurança para si mesma e para a equipe aonde quer que vá. Capaz de reviver aliados e rechaçar investidas agressivas, ela oferece um centro de calmaria em meio ao caos da batalha.',
     avatar:
-      'https://raw.githubusercontent.com/JV-Freitas/assets/main/valorant-player-valorant-sage-card.png',
+      'https://raw.githubusercontent.com/JV-Freitas/valorant-agents/main/public/assets/valorant-player-valorant-sage-card.png',
     isRadiant: true,
   },
   {
@@ -56,7 +56,7 @@ const characters = [
     ],
     bio: 'Nascido sob o eterno inverno das tundras russas, Sova rastreia, encontra e elimina inimigos com eficiência e precisão implacáveis. Seu arco personalizado e suas habilidades inigualáveis de reconhecimento garantem que nenhuma presa escape.',
     avatar:
-      'https://raw.githubusercontent.com/JV-Freitas/assets/main/valorant-player-valorant-sova-card.png',
+      'https://raw.githubusercontent.com/JV-Freitas/valorant-agents/main/public/assets/valorant-player-valorant-sova-card.png',
     isRadiant: false,
   },
   {
@@ -71,7 +71,7 @@ const characters = [
     ],
     bio: 'Representando a Coreia do Sul, sua terra natal, Jett tem um estilo de luta ágil e evasivo que permite que ela assuma riscos como ninguém. Ela corre em meio a qualquer confronto, cortando os inimigos antes mesmo que eles percebam quem ou o que os atingiu.',
     avatar:
-      'https://raw.githubusercontent.com/JV-Freitas/assets/main/valorant-player-valorant-jett-card.png',
+      'https://raw.githubusercontent.com/JV-Freitas/valorant-agents/main/public/assets/valorant-player-valorant-jett-card.png',
     isRadiant: true,
   },
   {
@@ -86,7 +86,7 @@ const characters = [
     ],
     bio: 'Killjoy, uma alemã genial, defende o campo de batalha facilmente usando seu arsenal de invenções. Se o dano causado por seu equipamento não der cabo dos inimigos, os efeitos negativos de seus queridos robôs dão conta do recado.',
     avatar:
-      'https://raw.githubusercontent.com/JV-Freitas/assets/main/valorant-player-valorant-killjoy-card.png',
+      'https://raw.githubusercontent.com/JV-Freitas/valorant-agents/main/public/assets/valorant-player-valorant-killjoy-card.png',
     isRadiant: false,
   },
   {
@@ -101,7 +101,7 @@ const characters = [
     ],
     bio: 'Vindo diretamente dos EUA, o arsenal orbital de Brimstone garante que o esquadrão dele esteja sempre em vantagem. Sua capacidade de oferecer utilidade com precisão a distância faz dele um comandante inigualável na linha de frente.',
     avatar:
-      'https://raw.githubusercontent.com/JV-Freitas/assets/main/valorant-player-valorant-brimstone-card.png',
+      'https://raw.githubusercontent.com/JV-Freitas/valorant-agents/main/public/assets/valorant-player-valorant-brimstone-card.png',
     isRadiant: false,
   },
   {
@@ -116,7 +116,7 @@ const characters = [
     ],
     bio: 'Raze chega do Brasil em uma explosão de carisma e armas letais. Com seu estilo de jogo "porradeiro", ela é craque em desentocar inimigos entrincheirados e limpar espaços apertados com uma bela dose de BUUUM!',
     avatar:
-      'https://raw.githubusercontent.com/JV-Freitas/assets/main/valorant-player-valorant-raze-card.png',
+      'https://raw.githubusercontent.com/JV-Freitas/valorant-agents/main/public/assets/valorant-player-valorant-raze-card.png',
     isRadiant: false,
   },
   {
@@ -131,7 +131,7 @@ const characters = [
     ],
     bio: 'Criada no coração do México, Reyna domina o combate individual, destacando-se a cada abate efetuado. Sua capacidade só é limitada por sua própria perícia, tornando-a bastante dependente de desempenho.',
     avatar:
-      'https://raw.githubusercontent.com/JV-Freitas/assets/main/valorant-player-valorant-reyna-card.png',
+      'https://raw.githubusercontent.com/JV-Freitas/valorant-agents/main/public/assets/valorant-player-valorant-reyna-card.png',
     isRadiant: true,
   },
   {
@@ -146,7 +146,7 @@ const characters = [
     ],
     bio: 'Viper, a química dos Estados Unidos, emprega uma variedade de dispositivos químicos venenosos para controlar o campo de batalha e prejudicar a visão do inimigo. Se as toxinas não matarem a presa, seus jogos mentais certamente o farão.',
     avatar:
-      'https://raw.githubusercontent.com/JV-Freitas/assets/main/valorant-player-valorant-viper-card.png',
+      'https://raw.githubusercontent.com/JV-Freitas/valorant-agents/main/public/assets/valorant-player-valorant-viper-card.png',
     isRadiant: false,
   },
   {
@@ -161,7 +161,7 @@ const characters = [
     ],
     bio: 'Mandando um salve direto da Austrália, Skye e sua equipe de feras desbravam territórios hostis. Com seus poderes de cura e suas criações que partem pra cima dos inimigos, qualquer equipe ficará mais forte e mais segura tendo Skye como aliada.',
     avatar:
-      'https://raw.githubusercontent.com/JV-Freitas/assets/main/valorant-player-valorant-skye-card.png',
+      'https://raw.githubusercontent.com/JV-Freitas/valorant-agents/main/public/assets/valorant-player-valorant-skye-card.png',
     isRadiant: true,
   },
 ];
